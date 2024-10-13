dcdcbe2446b30c6fdcd94147e57a344afd7f684b
link-qwerty <link.qwerty@gmail.com>
2024-10-13 18:22:01 +1200

    Added exr from module 1, ex 2

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+# Init
+quantity_of_exes = 12
+hours_spent = 1.5
+course_name = 'Python'
+
+# Code
+print('Курс:', course_name, '| Выполнено работ:', quantity_of_exes, '| Потрачено часов:', hours_spent, '| Среднее время выполнения задания:', hours_spent/quantity_of_exes)
