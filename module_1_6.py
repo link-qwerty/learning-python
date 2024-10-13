faa089f4cbc748bdfb73ad9b4039e3902a245377
link-qwerty <link.qwerty@gmail.com>
2024-10-13 20:25:51 +1200

    Added exr from module 1, ex 4

---
+++ module_1_6.py
@@ -0,0 +1,9 @@
+# Init
+my_string = input('Выедите произвольную строку: ')
+
+# Code
+print('Строка в верхнем регистре:', my_string.upper())
+print('Строка в нижнем регистре:', my_string.lower())
+print('Строка без пробелов:', my_string.replace(" ", ""))
+print('Первый символ строки:', my_string[0])
+print('Последниц символ строки:', my_string[-1])
