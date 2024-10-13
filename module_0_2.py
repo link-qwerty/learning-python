76958acf2fe485305fce9cda5af99c6ea7449d3f
link-qwerty <link.qwerty@gmail.com>
2024-10-13 17:20:50 +1200

    Added old lessons

---
+++ module_0_2.py
@@ -0,0 +1,18 @@
+# 1st program
+print("="*10, "1st program result", "="*10)
+print((9**0.5)*5)
+
+# 2nd program
+print("="*10, "2nd program result", "="*10)
+print(9.99>9.98 and 1000!=1000.1)
+
+# 3rd program
+print("="*10, "3rd program result", "="*10)
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+# 4th program
+print("="*10, "4th program result", "="*10)
+print(int((float('123.456')*10)%10))
+
