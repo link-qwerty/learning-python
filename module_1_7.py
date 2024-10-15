d294b860e3fbf992242912d3e70db80eb5140761
link-qwerty <link.qwerty@gmail.com>
2024-10-15 23:52:42 +1200

    Added exr from module 1, ex 5

--- module_1_7.py
+++ module_1_7.py
@@ -6,20 +6,15 @@ print(immutable_tuple)
 #immutable_tuple[0] = 2 -> immutable
 #immutable_tuple[3] = True -> immutable too
 #immutable_tuple[2] = 'spring' -> aaand... immutable!
-#immutable_tuple[2][1] = 'p' what?
 
 # let's try something else
 immutable_tuple = 1, 2, ["string"], False
 #immutable_tuple[2][0][1] = 'p' -> nope!
-immutable_tuple[2][0] = 'spring' # Ok!
+immutable_tuple[2][0] = 'spring' #Ok!
 print(immutable_tuple)
 
-immutable_tuple = 1, 2, ['s', 't', 'r', 'i', 'n', 'g'], False
-#immutable_tuple [2][0][1] = 'p' -> nope!
-print(immutable_tuple)
-
-immutable_tuple = 1, 2, [['s', 't', 'r', 'i', 'n', 'g']], False
-immutable_tuple [2][0][1] = 'p' # works! but ugly
+immutable_tuple = 1, 2, ['s', 't', 'ring', 'i', 'n', 'g'], False
+immutable_tuple [2][1] = 'p' #Ok!
 print(immutable_tuple)
 
 immutable_tuple = 1, 2, [0, 1], False
