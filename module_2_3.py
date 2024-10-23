3750777be12318c1c75109775eee606c9e42a760
link-qwerty <link.qwerty@gmail.com>
2024-10-23 13:47:26 +1200

    Added exr from module 2, ex 3

---
+++ module_2_3.py
@@ -0,0 +1,14 @@
+# Init
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+
+# Code
+while i < len(my_list):
+    if my_list[i] > 0:
+        print(my_list[i])
+    elif my_list[i] == 0:
+        i = i + 1
+        continue
+    else:
+        break
+    i = i + 1
