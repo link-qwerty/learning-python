c41778a396c28aa5ee66328c18facda6a251c37b
link-qwerty <link.qwerty@gmail.com>
2024-11-23 22:20:05 +1200

    Added exr from module 5, ex 2

--- module_5_2.py
+++ module_5_2.py
@@ -1,5 +1,6 @@
 # Defines
 class House:
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
@@ -13,8 +14,20 @@ class House:
         else:
             print("Такого этажа не существует")
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+
 # Code
-h1 = House('ЖК Горский', 25)
-h2 = House('Домик в деревне', 2)
-h1.go_to(20)
-h2.go_to(10)
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
