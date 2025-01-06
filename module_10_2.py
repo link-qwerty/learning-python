381882f8f90432f7c1d06b3f4b7e127561242e61
link-qwerty <link.qwerty@gmail.com>
2025-01-06 20:30:47 +1200

    Added exr from module 10, ex 2

---
+++ module_10_2.py
@@ -0,0 +1,81 @@
+# Defines
+from threading import Thread
+from time import sleep
+
+
+class Knight(Thread):
+    """
+    Класс потока Рыцарь
+
+    Объекты этого класса описывают некоего рыцаря (как-бы реконструктора, а иногда и просто сатаниста), который
+    сражается с воображаемыми врагами в воображаемом фэнтезийном мире
+
+    Методы класса
+    ---------------
+        __init__(self, name: str, power: int, weapon: str)
+            Конструктор: инициализация
+        fight(self)
+            Сражение с врагами
+        run(self)
+            Запуск потока
+
+    Атрибуты объекта
+    ---------------
+        :ivar str name: Имя рыцаря
+        :ivar int power: Сила рыцаря
+        :ivar str weapon: Оружие рыцаря
+    """
+
+    def __init__(self, name: str, power: int, weapon: str):
+        """
+        Конструктор: инициализация
+
+        Инициализация класса переданными аргументами
+        :param str name: Имя рыцаря
+        :param int power: Сила рыцаря
+        :param str weapon: Оружие рыцаря
+        """
+
+        Thread.__init__(self)
+        self.name = name
+        self.power = power
+        self.weapon = weapon
+
+    def fight(self):
+        """
+        Сражение с врагами
+
+        Метод запускает процесс боя. Рыцарь сражается до тех пор, пока не победит всех врагов
+        """
+
+        warriors = 100 - self.power
+        days = 0
+
+        while warriors > 0:
+            sleep(1)
+            days += 1
+            print(f'{self.name} сражается {days} дней(дня) используя {self.weapon}..., осталось '
+                  f'{warriors} ветряных мельниц.\n')
+            warriors -= self.power
+
+        print(f'{self.name} одержал победу спустя {days} дней(дня)! Благостно!')
+
+
+    def run(self):
+        """
+        Запуск потока
+
+        Метод запускает поток. Дополнительно может выполнять иные действия
+        """
+
+        print(f'{self.name}, на нас напали!')
+        self.fight()
+
+# Code
+first_knight = Knight('Сир Вёдер', 30, 'Кирпич')
+second_knight = Knight('Сир Артур', 20, 'Экскалибур')
+third_knight = Knight('Дон Кихот', 15, 'Копье')
+
+first_knight.start()
+second_knight.start()
+third_knight.start()
